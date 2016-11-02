db01ba11e3b5d5ffb515750baa5af88cd66c2e67
cutiegirl526 <kayladiniega@gmail.com>
2016-11-02 07:54:50 -1000

    Aliens

---
+++ index.js
@@ -0,0 +1,15 @@
+var blue = 1000;
+var green = 8000;
+var red = 1000;
+
+if (blue + green + red == 10000) {
+	if (green < 5000) {
+		console.log ("ugh why are they so ugly");
+	}
+	else if (green + red > 8000) {
+		console.log("It's starting to feel a lot like Christmas");
+	}
+	else if (red + blue > 3000) {
+		console.log ("Okay this seems somewhat managable. Wait no, we're still dead...");
+	}
+}
